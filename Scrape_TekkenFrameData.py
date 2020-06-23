4b5313c8daa3491ac7fbfdf9b8a66ebd1f08d351
Ian Peck <ianpeck22@gmail.com>
2020-06-23 17:07:10 -0500

    Initial Commit

--- Scrape_TekkenFrameData.py
+++ Scrape_TekkenFrameData.py
@@ -73,11 +73,3 @@ for fighter in fighter_linkparts:
 headings.append('Character')
 df.columns = headings
 df.to_csv('/Users/ianjpeck/Documents/tekken.csv', index = False)
-
-
-
-
-
-
-
-
---
+++ tekkenframedata.py
@@ -0,0 +1,6 @@
+# --- Imports ---
+import pandas as pd
+
+df = pd.read_csv('/Users/ianjpeck/Documents/tekken.csv')
+
+print(df)
